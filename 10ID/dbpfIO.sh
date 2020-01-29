4a99dda5f9cfb169ac4f2254bf568ed1c5391f9b
Kaz Gofron <kgofron@comcast.net>
2020-01-29 11:24:02 -0500

    the ioc startup description

---
+++ 10ID/dbpfIO.sh
@@ -0,0 +1,46 @@
+!/bin/bash
+
+#E1212 DI
+dbpf XF:10ID-ES{IO:1}DI:1-Sts.DESC "DI Channel 1"
+dbpf XF:10ID-ES{IO:1}DI:2-Sts.DESC "DI Channel 2"
+dbpf XF:10ID-ES{IO:1}DI:3-Sts.DESC "DI Channel 3"
+dbpf XF:10ID-ES{IO:1}DI:4-Sts.DESC "DI Channel 4"
+dbpf XF:10ID-ES{IO:1}DI:5-Sts.DESC "DI Channel 5"
+dbpf XF:10ID-ES{IO:1}DI:6-Sts.DESC "DI Channel 6"
+dbpf XF:10ID-ES{IO:1}DI:7-Sts.DESC "DI Channel 7"
+dbpf XF:10ID-ES{IO:1}DI:8-Sts.DESC "DI Channel 8"
+#E1212 DO
+dbpf XF:10ID-ES{IO:1}DO:1-Cmd.DESC "DO Channel 1"
+dbpf XF:10ID-ES{IO:1}DO:2-Cmd.DESC "DO Channel 2"
+dbpf XF:10ID-ES{IO:1}DO:3-Cmd.DESC "DO Channel 3"
+dbpf XF:10ID-ES{IO:1}DO:4-Cmd.DESC "DO Channel 4"
+dbpf XF:10ID-ES{IO:1}DO:5-Cmd.DESC "DO Channel 5"
+dbpf XF:10ID-ES{IO:1}DO:6-Cmd.DESC "DO Channel 6"
+dbpf XF:10ID-ES{IO:1}DO:7-Cmd.DESC "DO Channel 7"
+dbpf XF:10ID-ES{IO:1}DO:8-Cmd.DESC "DO Channel 8"
+
+#E1214 DI
+dbpf XF:10ID-ES{IO}DI:1-Sts.DESC "DI Channel 1"
+dbpf XF:10ID-ES{IO}DI:2-Sts.DESC "DI Channel 2"
+dbpf XF:10ID-ES{IO}DI:3-Sts.DESC "DI Channel 3"
+dbpf XF:10ID-ES{IO}DI:4-Sts.DESC "DI Channel 4"
+dbpf XF:10ID-ES{IO}DI:5-Sts.DESC "DI Channel 5"
+dbpf XF:10ID-ES{IO}DI:6-Sts.DESC "DI Channel 6"
+#E1214 DO
+dbpf XF:10ID-ES{IO}DO:1-Cmd.DESC "Relay Channel 1"
+dbpf XF:10ID-ES{IO}DO:2-Cmd.DESC "Relay Channel 2"
+dbpf XF:10ID-ES{IO}DO:3-Cmd.DESC "Relay Channel 3"
+dbpf XF:10ID-ES{IO}DO:4-Cmd.DESC "Relay Channel 4"
+dbpf XF:10ID-ES{IO}DO:5-Cmd.DESC "Relay Channel 5"
+dbpf XF:10ID-ES{IO}DO:6-Cmd.DESC "Relay Channel 6"
+
+#E1262
+dbpf XF:10ID-ES{IO:TC}T:1-I.DESC "TC Channel 1"
+dbpf XF:10ID-ES{IO:TC}T:2-I.DESC "TC Channel 2"
+dbpf XF:10ID-ES{IO:TC}T:3-I.DESC "TC Channel 3"
+dbpf XF:10ID-ES{IO:TC}T:4-I.DESC "TC Channel 4"
+dbpf XF:10ID-ES{IO:TC}T:5-I.DESC "TC Channel 5"
+dbpf XF:10ID-ES{IO:TC}T:6-I.DESC "TC Channel 6"
+dbpf XF:10ID-ES{IO:TC}T:7-I.DESC "TC Channel 7"
+dbpf XF:10ID-ES{IO:TC}T:8-I.DESC "TC Channel 8"
+
